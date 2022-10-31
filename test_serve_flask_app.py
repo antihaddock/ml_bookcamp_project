d857337434544ad52eedc02f8af7a509c4ff7160
antihaddock <antihaddock1@gmail.com>
2022-10-31 21:03:24 +1100

    adding all flask files

---
+++ test_serve_flask_app.py
@@ -0,0 +1,23 @@
+
+# testing the Flask App works locally when predict.py is running
+# Test by iterating over all rows of the test dataframe
+
+import requests
+import pandas as pd
+
+url = 'http://172.26.3.210:9696/predict_outcome'
+
+df = pd.read_csv('./Data/deployment_test_data.csv')
+
+for i in range(len(df)):
+    client = df.iloc[i].to_dict()
+
+    response = requests.post(url, json=client)
+    result = response.json()
+    print(result)
+
+
+
+
+
+
